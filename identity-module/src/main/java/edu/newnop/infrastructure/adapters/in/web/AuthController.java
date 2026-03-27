b99f010c58ba0c4e9cddaefe3a5f517cfcca0753
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-03-27 16:59:11 +0530

    feat(identity): expose OTP endpoints in AuthController
    - Add '/send-otp' endpoint to trigger a new verification code.
    - Add '/verify' endpoint to validate user-submitted OTPs.
    - Configure request matching, input validation via @Valid

--- identity-module/src/main/java/edu/newnop/infrastructure/adapters/in/web/AuthController.java
+++ identity-module/src/main/java/edu/newnop/infrastructure/adapters/in/web/AuthController.java
@@ -2,16 +2,16 @@ package edu.newnop.infrastructure.adapters.in.web;
 
 import edu.newnop.application.port.in.LoginUseCase;
 import edu.newnop.application.port.in.RegisterUserUseCase;
+import edu.newnop.application.port.in.RequestOTPMailUseCase;
 import edu.newnop.common.ApiResponse;
+import edu.newnop.infrastructure.adapters.in.web.dto.EmailVerificationRequest;
 import edu.newnop.infrastructure.adapters.in.web.dto.LoginRequest;
+import edu.newnop.infrastructure.adapters.in.web.dto.OTPMailRequest;
 import edu.newnop.infrastructure.adapters.in.web.dto.RegisterRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,19 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final RegisterUserUseCase registerUseCase;
     private final LoginUseCase loginUseCase;
+    private final RequestOTPMailUseCase requestOTPMailUseCase;
 
     @PostMapping("/register")
-    public ApiResponse<RegisterUserUseCase.RegistrationResult> register(@RequestBody @Valid RegisterRequest request) {
-        var result = registerUseCase.register(
-                new RegisterUserUseCase.RegisterCommand(
-                        request.getName(),
-                        request.getEmail(),
-                        request.getPassword(),
-                        request.getRole()
-                )
-        );
-        String message = request.getRole() + " created successfully";
-        return ApiResponse.success(201, message, result);
+    public ApiResponse<RegisterUserUseCase.RegistrationResult<RequestOTPMailUseCase.RequestOTPMailResult>> register(@RequestBody @Valid RegisterRequest request) {
+        return ApiResponse.success(
+                201,
+                "Sign Up successfully",
+                registerUseCase.register(
+                        new RegisterUserUseCase.RegisterCommand(
+                                request.getName(),
+                                request.getEmail(),
+                                request.getPassword(),
+                                request.getRole()
+                        )
+                ));
 
     }
 
@@ -45,6 +47,32 @@ public class AuthController {
                                 request.getEmail(),
                                 request.getPassword()
                         ))
+        );
+    }
+
+    @GetMapping("/send-otp")
+    public ApiResponse<RequestOTPMailUseCase.RequestOTPMailResult> requestOtp(@RequestBody @Valid OTPMailRequest request) {
+        return ApiResponse.success(
+                HttpStatus.OK.value(),
+                "OTP sent successfully",
+                requestOTPMailUseCase.generateAndSendVerificationMail(
+                        new RequestOTPMailUseCase.RequestOTPMailCommand(
+                                request.getEmail()
+                        )
+                )
+        );
+    }
+
+    @PostMapping("/verify")
+    public ApiResponse<Void> verifyEmail(@RequestBody @Valid EmailVerificationRequest request) {
+        registerUseCase.verifyEmail(new RegisterUserUseCase.VerifyEmailCommand(
+                request.getOtpId(),
+                request.getOtpCode()
+        ));
+        return ApiResponse.success(
+                HttpStatus.PERMANENT_REDIRECT.value(),
+                "Email verified successfully",
+                null
         );
     }
 }
