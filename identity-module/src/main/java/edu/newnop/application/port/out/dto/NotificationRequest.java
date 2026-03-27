0e96078b1780c8d795634afea3e71197f1f4fab1
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-03-27 16:47:06 +0530

    feat: integrate SMTP mail service and notification infrastructure
    Implements a decoupled notification layer
    and Spring Boot's JavaMailSender integration.
    
    - Add 'spring-boot-starter-mail' dependency to 'pom.xml'.
    - Define 'NotificationRequest' DTO for standardized cross-service communication.
    - Implement 'SmtpEmailAdapter' using 'JavaMailSender' for Mail delivery.
    - Establish Port and Adapter interfaces to abstract email provider logic.
    - Configure externalized SMTP settings in 'application.yml'

---
+++ identity-module/src/main/java/edu/newnop/application/port/out/dto/NotificationRequest.java
@@ -0,0 +1,18 @@
+package edu.newnop.application.port.out.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationRequest<T> {
+    private String receiverEmail;
+    private String receiverName;
+    private String message;
+    private String subject;
+    private T data;
+}
