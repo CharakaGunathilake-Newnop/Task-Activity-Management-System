e34f93c37b6f94abcf09426b638e77b3d0f8878a
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-03-30 14:54:24 +0530

    feat #2(identity): custom handler UsernameNotFoundException in GlobalExceptionHandler

--- infrastructure/src/main/java/edu/newnop/web/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/edu/newnop/web/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import edu.newnop.infrastructure.adapters.in.web.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,6 +29,17 @@ public class GlobalExceptionHandler {
                         "Bad Request",
                         null,
                         errors
+                ));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ApiError<Void>> handleUserNotFoundError(UsernameNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiError.error(
+                        "User Not Found",
+                        ex.getMessage(),
+                        null
                 ));
     }
 
