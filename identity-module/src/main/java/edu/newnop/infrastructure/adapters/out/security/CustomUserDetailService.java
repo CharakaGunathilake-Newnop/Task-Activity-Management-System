8f13775b6d2389fd4229f7353368eeab19ab6fdc
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-03-30 20:28:05 +0530

    feat #5: introduce custom AuthenticatedUser to handle attributes across modules

--- identity-module/src/main/java/edu/newnop/infrastructure/adapters/out/security/CustomUserDetailService.java
+++ identity-module/src/main/java/edu/newnop/infrastructure/adapters/out/security/CustomUserDetailService.java
@@ -1,5 +1,6 @@
 package edu.newnop.infrastructure.adapters.out.security;
 
+import edu.newnop.common.security.AuthenticatedUser;
 import edu.newnop.domain.model.User;
 import edu.newnop.infrastructure.adapters.out.persistence.PostgresUserAdapter;
 import lombok.RequiredArgsConstructor;
@@ -16,19 +17,16 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        // Load user from database using email
-        User user = userRepository.findByEmail(email).orElseThrow(
+        // Load user and return from database using email
+        final User user = userRepository.findByEmail(email).orElseThrow(
                 () -> new UsernameNotFoundException("User not found by email: " + email)
         );
 
-        // Convert your User entity to Spring Security's UserDetails
-        return new org.springframework.security.core.userdetails.User(
+        return new AuthenticatedUser(
+                user.getId(),
+                user.getName(),
                 user.getEmail(),
-                user.getPassword(),
-                user.isEnabled(),
-                user.isAccountNonExpired(),
-                user.isCredentialsNonExpired(),
-                user.isAccountNonLocked(),
-                user.getAuthorities());
+                user.getAuthorities()
+        );
     }
 }
