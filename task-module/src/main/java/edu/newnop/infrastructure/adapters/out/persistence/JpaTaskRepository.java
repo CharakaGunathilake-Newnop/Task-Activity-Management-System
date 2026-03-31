e211c3abcc830f8ddd672900aadcf8c0d345269d
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-04-01 02:06:48 +0530

    fix (task): fix query "assignedUserId" field name mismatch

--- task-module/src/main/java/edu/newnop/infrastructure/adapters/out/persistence/JpaTaskRepository.java
+++ task-module/src/main/java/edu/newnop/infrastructure/adapters/out/persistence/JpaTaskRepository.java
@@ -10,16 +10,16 @@ import java.util.Optional;
 
 @Repository
 public interface JpaTaskRepository extends JpaRepository<TaskEntity, Long> {
-    Page<TaskEntity> findAllByUserId(Long aLong, PageRequest pageRequest);
+    Page<TaskEntity> findAllByAssignedUserId(Long assignedUserId, PageRequest pageRequest);
 
-    Optional<TaskEntity> findByIdAndUserId(Long taskId, Long userId);
+    Optional<TaskEntity> findByIdAndAssignedUserId(Long taskId, Long assignedUserId);
 
     // Custom query to search by title, status, or priority for a specific user with pagination
     @Query("SELECT t FROM tasks t WHERE t.assignedUserId = :userId AND " +
             "(LOWER(t.title) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR " +
             "LOWER(t.status) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR " +
             "LOWER(t.priority) LIKE LOWER(CONCAT('%', :searchQuery, '%')))")
-    Page<TaskEntity> findByUserIdAndSearchQuery(Long aLong, String searchQuery, PageRequest pageRequest);
+    Page<TaskEntity> findByAssignedUserIdAndSearchQuery(Long assignedUserId, String searchQuery, PageRequest pageRequest);
 
     // Custom query to search by title, status, or priority for all tasks with pagination
     @Query("SELECT t FROM tasks t WHERE " +
