0965d368e52daf46d4a59089d344ae7c7159f476
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-04-01 02:01:15 +0530

    feat (task): implement endpoints with request and response DTOs for get, update delete functionalities

--- task-module/src/main/java/edu/newnop/infrastructure/adapters/in/web/TaskController.java
+++ task-module/src/main/java/edu/newnop/infrastructure/adapters/in/web/TaskController.java
@@ -2,15 +2,21 @@ package edu.newnop.infrastructure.adapters.in.web;
 
 
 import edu.newnop.application.in.CreateTaskUseCase;
+import edu.newnop.application.in.DeleteTaskUseCase;
+import edu.newnop.application.in.GetTasksUseCase;
+import edu.newnop.application.in.UpdateTaskUseCase;
 import edu.newnop.common.ApiResponse;
 import edu.newnop.infrastructure.adapters.in.web.dto.CreateTaskRequest;
+import edu.newnop.infrastructure.adapters.in.web.dto.UpdateTaskRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 
 @RestController
@@ -18,6 +24,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/tasks")
 public class TaskController {
     private final CreateTaskUseCase createTaskUseCase;
+    private final GetTasksUseCase getTasksUseCase;
+    private final UpdateTaskUseCase updateTaskUseCase;
+    private final DeleteTaskUseCase deleteTaskUseCase;
+
 
     @PostMapping
     @PreAuthorize("hasRole('USER')")
@@ -33,6 +43,78 @@ public class TaskController {
                                 request.getPriority(),
                                 request.getDueDate()
                         )
+                )
+        );
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ApiResponse<GetTasksUseCase.GetTasksResult> getTasks(
+            @PageableDefault(page = 1, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) Long selectedUserId) {
+        return ApiResponse.success(
+                200,
+                "Tasks retrieved successfully",
+                getTasksUseCase.getPaginatedTasks(
+                        new GetTasksUseCase.GetTasksCommand(
+                                pageable.getPageNumber(),
+                                pageable.getPageSize(),
+                                pageable.getSort().isSorted() ? pageable.getSort().toString() : "createdAt",
+                                pageable.getSort().isSorted() ? pageable.getSort().toString() : Sort.Direction.DESC.name(),
+                                search,
+                                selectedUserId
+                        )
+                )
+        );
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse<UpdateTaskUseCase.UpdateTaskResult> updateTask(
+            @PathVariable Long id,
+            @RequestBody @Valid UpdateTaskRequest request) {
+        return ApiResponse.success(
+                200,
+                String.format("Task with id %d updated successfully", id),
+                updateTaskUseCase.updateTask(
+                        new UpdateTaskUseCase.UpdateTaskCommand(
+                                id,
+                                request.getTitle(),
+                                request.getDescription(),
+                                request.getStatus(),
+                                request.getPriority(),
+                                request.getDueDate()
+                        )
+                )
+        );
+    }
+
+    @PatchMapping("/{id}/status")
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse<UpdateTaskUseCase.UpdateTaskStatusResult> updateTaskStatus(
+            @PathVariable Long id,
+            @RequestParam String status) {
+        return ApiResponse.success(
+                200,
+                String.format("Status of task with id %d updated successfully", id),
+                updateTaskUseCase.updateTaskStatus(
+                        new UpdateTaskUseCase.UpdateTaskStatusCommand(
+                                id,
+                                status
+                        )
+                )
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse<DeleteTaskUseCase.DeleteTaskResult> deleteTask(@PathVariable Long id) {
+        return ApiResponse.success(
+                200,
+                String.format("Task with id %d deleted successfully", id),
+                deleteTaskUseCase.deleteTask(
+                        new DeleteTaskUseCase.DeleteTaskCommand(id)
                 )
         );
     }
---
+++ task-module/src/main/java/edu/newnop/infrastructure/adapters/in/web/dto/UpdateTaskRequest.java
@@ -0,0 +1,29 @@
+package edu.newnop.infrastructure.adapters.in.web.dto;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateTaskRequest {
+    @Size(min = 1, max = 50, message = "Name must be 1–50 characters")
+    @Pattern(
+            regexp = "^(?! )(?!.*  )[\\p{L} ]+(?<! )$",
+            message = "Title can contain only letters and single spaces, without leading, trailing, or repeated spaces"
+    )
+    private String title;
+    @Size(max = 200, message = "Description must be up to 200 characters")
+    private String description;
+    private String status;
+    private String priority;
+    private Date dueDate;
+}
+
