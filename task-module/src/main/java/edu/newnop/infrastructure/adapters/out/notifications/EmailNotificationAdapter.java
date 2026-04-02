68db3c9f7721462a76bfa7e3e0449eb484677eb1
Charaka Gunathilake <cgunathilake@newnop.net>
2026-04-02 17:55:23 +0530

    Implement task notification events (#8)
    * Stop tracking tm_app_be.pem
    
    * chore #5(task): introduce "common-module" for tasks
    
    * feat #5(task): implement task domain with base domain entity with use cases, service, repository port
    
    * feat #5(task): implement data persistence for Task with repository port, PostgresTaskAdapter, Entity and Mapper
    
    * feat #5(task): implement task controller with create request dto and endpoint
    
    * feat #5(task): implement email notification port, adapter and DTO
    
    * feat #5: introduce custom AuthenticatedUser to handle attributes across modules
    
    * fix #5: remove invalid validation or Date type in CreateTaskRequest
    
    * refactor #5(task): include task priority mapping to "toDomain" mapper
    
    * refactor #5(task): rename BaseJpaEntity to TaskBaseJpaEntity
    
    * refactor #5(task): change date type of "dueDate" to Date to handle proper date formats
    
    * feat #5(task): introduce RBAC to create task endpoint
    
    * feat #5(task): include missing priority field domain to entity mapper
    
    * feat #5(task): remove assigneeUserId from CreateTaskUseCase to handle in service
    
    * feat #5(task): complete "create task" service logic with notification trigger
    
    * feat #5(task): implement task domain with base domain entity with use cases, service, repository port
    
    # Conflicts:
    #	task-module/src/main/java/edu/newnop/application/in/CreateTaskUseCase.java
    #	task-module/src/main/java/edu/newnop/application/services/TaskServiceImpl.java
    #	task-module/src/main/java/edu/newnop/domain/model/Task.java
    
    * refactor #5(task): rename BaseJpaEntity to TaskBaseJpaEntity
    
    * feat (task): introduce Get, Update, Delete tasks use cases
    
    * refactor (task): generalize notification adapter method name suitable for tasks related events
    
    * feat (task): implement search/paginated get, update delete task service logic crud with repo custom queries and methods for persistence
    
    * feat (task): implement endpoints with request and response DTOs for get, update delete functionalities
    
    * fix (task): fix query "assignedUserId" field name mismatch
    
    * fix (task): fix missing password field user for authentication in CustomUserDetailService and AuthenticatedUser
    
    * refactor (task): combine the pagination request object to "PageRequest" for get and remove redundant url mappers
    
    * feat (task): introduce authentication and authorization custom error handlers in GlobalExceptionHandler
    
    * fix (task): fix incorrect "assignedUserId" field name in custom query in JpaTaskRepository
    
    * feat (task): include "assignedUserId" to TaskResponseDto to explicitly identify task owner
    
    * refactor (task): change the "get" service method to support changes made to request and response behaviors
    
    * feat (task): connect notification sending mechanism for events triggered by Task interactions
    
    * feat (task): implement custom EmailServiceUnavailableException for Task module
    
    * feat #5(task): implement task domain with base domain entity with use cases, service, repository port
    
    # Conflicts:
    #	task-module/src/main/java/edu/newnop/application/in/CreateTaskUseCase.java
    #	task-module/src/main/java/edu/newnop/application/out/TaskRepositoryPort.java
    #	task-module/src/main/java/edu/newnop/application/services/TaskService.java
    #	task-module/src/main/java/edu/newnop/application/services/TaskServiceImpl.java
    #	task-module/src/main/java/edu/newnop/domain/model/Task.java
    
    * refactor #5(task): rename BaseJpaEntity to TaskBaseJpaEntity
    
    * feat #5(task): implement task domain with base domain entity with use cases, service, repository port
    
    # Conflicts:
    #	task-module/src/main/java/edu/newnop/application/in/CreateTaskUseCase.java
    #	task-module/src/main/java/edu/newnop/application/services/TaskServiceImpl.java
    #	task-module/src/main/java/edu/newnop/domain/model/Task.java
    
    * refactor #5(task): rename BaseJpaEntity to TaskBaseJpaEntity

--- task-module/src/main/java/edu/newnop/infrastructure/adapters/out/notifications/EmailNotificationAdapter.java
+++ task-module/src/main/java/edu/newnop/infrastructure/adapters/out/notifications/EmailNotificationAdapter.java
@@ -2,15 +2,55 @@ package edu.newnop.infrastructure.adapters.out.notifications;
 
 import edu.newnop.application.out.TaskNotificationPort;
 import edu.newnop.application.out.dto.NotificationRequest;
+import edu.newnop.infrastructure.adapters.in.web.dto.TaskResponseDto;
+import edu.newnop.infrastructure.adapters.out.notifications.exceptions.EmailServiceUnavailableException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.StringFormattedMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class EmailNotificationAdapter implements TaskNotificationPort {
 
-    @Override
-    public void notifyTaskEvent(NotificationRequest<?> request) {
+    private final JavaMailSender mailSender;
 
+    @Value("${application.notification.sender.mail}")
+    private String senderMail;
+
+    @Override
+    public void notifyTaskEvent(NotificationRequest<?> notificationRequest) {
+        final Object data = notificationRequest.getData();
+        final String receiver = notificationRequest.getReceiverEmail().trim();
+        final String receiverName = notificationRequest.getReceiverName().trim();
+
+        // Use \n for newlines and \t for tabs
+        final StringFormattedMessage formattedData = new StringFormattedMessage("Task\n\t ID: %s\n\tTitle: %s\n\tDescription: %s",
+                ((TaskResponseDto) data).getId(),
+                ((TaskResponseDto) data).getTitle(),
+                ((TaskResponseDto) data).getDescription()
+        );
+        final String body = "Hi " + receiverName + ",\n\n" +
+                notificationRequest.getMessage() + "\n\t" +
+                formattedData;
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(senderMail);
+        mailMessage.setTo(receiver);
+        mailMessage.setSubject(notificationRequest.getSubject());
+        mailMessage.setText(body);
+
+        log.debug("Sending Mail from: {} to: {}", senderMail, receiver);
+
+        try {
+            mailSender.send(mailMessage);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}: {}", receiver, e.getMessage());
+            throw new EmailServiceUnavailableException("Email service unavailable. Please try again later.");
+        }
     }
 }
