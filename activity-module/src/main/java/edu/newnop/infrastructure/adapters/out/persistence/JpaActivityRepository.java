e7f8b7a38057231d03f837ac85fb234e1463a4d0
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-04-06 10:57:05 +0530

    refactor (activity): renamed the "ActivityRepository" to more specific "JpaActivityRepository"

--- activity-module/src/main/java/edu/newnop/infrastructure/adapters/out/persistence/JpaActivityRepository.java
+++ activity-module/src/main/java/edu/newnop/infrastructure/adapters/out/persistence/JpaActivityRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<ActivityEntity, Long> {
+public interface JpaActivityRepository extends JpaRepository<ActivityEntity, Long> {
 }
